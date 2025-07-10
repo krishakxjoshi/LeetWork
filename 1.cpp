7ea0bfa4cc761fca12e508d6bc3d6d996893ca4b
Krishak Joshi <krishakxjoshi@gmail.com>
2025-07-10 20:30:35 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        int p=0,q=0;
+       
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){for(int i=0;i<n;i++)nums.pop_back();nums.push_back(i);nums.push_back(j);return nums;}
+                
+            }
+        }
+  return nums;  }
+};
