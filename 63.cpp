7ec1262dc105815871c2d4e4abb5a4642a40b2d1
Krishak Joshi <krishakxjoshi@gmail.com>
2025-09-05 23:36:16 +0530

    Add files via upload

---
+++ 63.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& arr) {
+        int m = arr.size(), n = arr[0].size();
+        if (arr[0][0] == 1) return 0;
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for (int i = 0; i < m; i++) {
+            if (arr[i][0] == 1) break;
+            dp[i][0] = 1;
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (arr[0][j] == 1) break;
+            dp[0][j] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (arr[i][j] == 1)
+                    dp[i][j] = 0;
+                else
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+};
