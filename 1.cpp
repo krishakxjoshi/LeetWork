579a37cf132c0e484cb8e138658b9b6cd834348a
Krishak Joshi <krishakxjoshi@gmail.com>
2026-02-19 23:47:18 +0530

    Refactor Solution class for twoSum function

--- 1.cpp
+++ 1.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) { 
         int n=nums.size();
         int p=0,q=0;
-       
+        
         for(int i=0;i<n;i++){ 
             for(int j=i+1;j<n;j++){
                 if(nums[i]+nums[j]==target){for(int i=0;i<n;i++)nums.pop_back();nums.push_back(i);nums.push_back(j);return nums;}
@@ -13,6 +13,7 @@ public:
   return nums;  }
 
 };
+
 
 
 
