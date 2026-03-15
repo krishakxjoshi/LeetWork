8a93e93a0d7ac3a7c9743be4a4fa210e3f6b4d85
Krishak Joshi <krishakxjoshi@gmail.com>
2026-03-15 23:08:04 +0530

    Refactor twoSum function in Solution class

--- 1.cpp
+++ 1.cpp
@@ -7,7 +7,7 @@ public:
         for(int i=0;i<n;i++){ 
             for(int j=i+1;j<n;j++){
                 if(nums[i]+nums[j]==target){for(int i=0;i<n;i++)nums.pop_back();nums.push_back(i);nums.push_back(j);return nums;}
-                
+                 
             }
         } 
   return nums;  }
