b274d9da94f52af8593852d03bf39df184533f95
Krishak Joshi <krishakxjoshi@gmail.com>
2026-08-27 23:10:01 +0530

    Create 1475.cpp

---
+++ 1475.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        stack<int>st;
+        for(int i=0;i<prices.size();i++){
+            if(st.empty() || prices[st.top()]<prices[i])st.push(i);
+            else{
+                while(!st.empty() && prices[st.top()]>=prices[i]){
+                    prices[st.top()]-=prices[i];
+                    st.pop();
+                }
+                st.push(i);
+            }
+        }
+        return prices;
+    }
+};
